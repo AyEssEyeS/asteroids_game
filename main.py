02eb6659432bee5563f5794114ed1169c625522b
AyEssEyeS <stefanalbu2002@yahoo.com>
2026-03-02 15:37:51 +0200

    B. Added Triangle player to the middle of the screen

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 def main():
+    my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while (True):
         log_state()
         for event  in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        my_player.draw(screen)
         pygame.display.flip()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+        
+
     
 
 if __name__ == "__main__":
     main()
-
