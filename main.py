bed76aa2616fa7fd3427569ecb8833a3c286b3f7
AyEssEyeS <stefanalbu2002@yahoo.com>
2026-03-03 18:53:53 +0200

    F. Added bullets

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from circleshape import CircleShape
+from shot import Shot
 
 def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    #New shots group 
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers  = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+   
     asteroid_field = AsteroidField()
     my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
